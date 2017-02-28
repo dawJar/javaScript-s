d72a9c1aced8f594f461a9a09eff6e8e9786e94c
dawJar <dawid.jaracz@gmail.com>
2017-02-28 02:21:25 +0100

    ajax complete

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+(function() {
 
+    var container = document.getElementById('app');
+    var button = document.getElementById('fetch-data');
+    var currentJokeContainer = document.getElementById('current-joke');
+    var latestJokesContainer = document.getElementById('latest-jokes');
+    var currentJoke;
+    var request = new XMLHttpRequest();
+    var url = 'http://api.icndb.com/jokes/random';
+
+    var fetchJoke = function () {
+        request.onreadystatechange = function () {
+            if (this.readyState == XMLHttpRequest.DONE) {
+                if (request.status == 200) {
+                    setChuckJoke(this);
+                } else {
+                    console.error('cannot fetch data');
+                }
+            }
+        };
+        request.open('GET', url, true);
+        request.send();
+    };
+
+    var setChuckJoke = function (e) {
+        var json = JSON.parse(e.responseText)
+        var chuckJoke = json.value.joke;
+
+        currentJokeContainer.innerHTML = chuckJoke;
+        addToLatestJokes(chuckJoke);
+        currentJoke = chuckJoke;
+    };
+
+    var addToLatestJokes = function (joke) {
+        var newParagraph = document.createElement('p');
+        newParagraph.className = 'latest-paragraph';
+        newParagraph.innerHTML = joke;
+        latestJokesContainer.appendChild(newParagraph);
+    };
+
+    button.addEventListener('click', fetchJoke);
+
+})();
